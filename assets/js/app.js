9a6bf9483d43469c28b442376a9f6832231370ad
Jake Jarrett <jakeryanjarrett@gmail.com>
2016-03-10 21:42:59 +1100

    Add notifications & some more fixes
    Notifications now supported
    Using constant for messageInput dom
    
    Some improvements to html & css

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,13 +3,17 @@
  *
  * @Author - Jake Jarrett
  * @GitHub URL - https://github.com/jakejarrett/chat-example
- *
  */
+
+"use strict";
 
 $(function() {
 
+    /** Setup App's variables **/
+    const messageInput = $("#message");
+
     /** Focus the message input when we"ve loaded the page, So users can just start chatting! **/
-    $("#message").focus();
+    messageInput.focus();
 
     /**
      * Initialize Socket.io :) this is the secret sauce to the entire app!
@@ -41,12 +45,12 @@ $(function() {
         e.preventDefault();
 
         /** Check if the input actually has stuff in it (EG/ not just a bunch of spaces) **/
-        if (0 !== $("#message").val().trim().length) {
+        if (0 !== messageInput.val().trim().length) {
             /** Let the app know we want to send the message **/
-            socket.emit("sendchat", $("#message").val());
+            socket.emit("sendchat", messageInput.val());
 
             /** Clear the input **/
-            $("#message").val("");
+            messageInput.val("");
         }
 
         /** Always return false **/
@@ -61,6 +65,10 @@ $(function() {
 
         /** Scroll to the bottom of the chat ~ **/
         $("html, body").animate({ scrollTop: $(document).height() });
+
+        notifyUser("New Message", {
+            body: msg
+        });
     });
 
     /**
@@ -69,18 +77,66 @@ $(function() {
      * NOTE- this is only for development purposes right now, will be removed later.
      */
     $("html").on("click", function (e) {
-        $('#message').focus();
+        messageInput.focus();
     });
 
     /**
-     * We don't want a user to run random scripts in the page, so lets escape all messages.
+     * Escape HTML
+     *  We don't want a user to run random scripts in the page, so lets escape all messages.
      *
-     * @param string
+     * Based on Mustache's code
+     * @Mustache.js - https://github.com/janl/mustache.js/blob/master/mustache.js#L60
+     *
+     * @param {string} string
      * @returns {string}
      */
-    function escapeHtml(string) {
+    var escapeHtml = function(string) {
+
+        /** Array of entities we will escape **/
+        let entityMap = {
+            "&": "&amp;",
+            "<": "&lt;",
+            ">": "&gt;",
+            '"': '&quot;',
+            "'": '&#39;',
+            "/": '&#x2F;'
+        };
+
+        /** Return the escaped string **/
         return String(string).replace(/[&<>"'\/]/g, function (s) {
             return entityMap[s];
         });
-    }
+    };
+
+    /**
+     * Simple function to present a notification to the user when called
+     *
+     * @param {string} title
+     * @param {object} options
+     * @returns {boolean}
+     */
+    var notifyUser = function(title, options) {
+        /**
+         * If the user has given us permission to use notifications, Lets notify them of a new message!
+         */
+        if ("granted" === Notification.permission) {
+            var notification = new Notification(title, options);
+        }
+        /**
+         * If they haven't said no yet, Lets ask before ending the function.
+         */
+        else if ("denied" !== Notification.permission) {
+            Notification.requestPermission(function (permission) {
+                /** If the user agrees to notifications, Lets setup a new notification. **/
+                if (permission === "granted") {
+                    var notification = new Notification(title, options);
+                }
+            });
+        }
+
+        /**
+         * Because the user doesn't want notifications, We will exit the function now.
+         */
+        return false;
+    };
 });
