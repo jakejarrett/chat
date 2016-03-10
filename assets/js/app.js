9f62341634ea6d916ca23f4e19951c619d3a3cf9
Jake Jarrett <jake@vokke.com.au>
2016-03-10 14:13:48 +1100

    Responsive Fixes & scroll whenever a new message is sent
    Seeing the latest message is cool too ~~

--- assets/js/app.js
+++ assets/js/app.js
@@ -45,6 +45,9 @@ $(function() {
             /** Let the app know we want to send the message **/
             socket.emit("sendchat", $("#message").val());
 
+            /** Scroll to the bottom of the chat ~ **/
+            $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+
             /** Clear the input **/
             $("#message").val("");
         }
