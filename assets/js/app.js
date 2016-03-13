08be0d6936e0ead869429225c4c3201f4efdc760
Jake Jarrett <jakeryanjarrett@gmail.com>
2016-03-13 15:31:35 +1100

    Preserve New lines & can press enter to submit message
    Now you can press enter to submit and press control/shift enter to add new line.
    
    Preserves your breaks/new lines in the message. :)

--- assets/js/app.js
+++ assets/js/app.js
@@ -41,27 +41,26 @@ $(function() {
      * When the form is submitted, We will want to show the users message on the screen :)
      */
     $("form").submit(function (e) {
-        /** Prevent the form from submitting **/
-        e.preventDefault();
+       sendMessage(e);
+    });
 
-        /** Check if the input actually has stuff in it (EG/ not just a bunch of spaces) **/
-        if (0 !== messageInput.val().trim().length) {
-            /** Let the app know we want to send the message **/
-            socket.emit("sendchat", messageInput.val());
-
-            /** Clear the input **/
-            messageInput.val("");
+    /**
+     * When a user clicks Enter on the textarea, Lets instead make that send the message.
+     */
+    messageInput.keydown(function(event){
+        if(event.keyCode == 13 && event.ctrlKey) {
+            event.preventDefault();
+            messageInput.val( messageInput.val() + "\n");
+        } else if (event.keyCode == 13 && !event.shiftKey) {
+            sendMessage(event);
         }
-
-        /** Always return false **/
-        return false;
     });
 
     /**
      * The app will tell us when to update the view (Socket.io) and it will also give us the data to put there.
      */
     socket.on("updatechat", function (msg) {
-        $("#messageContainer").append(htmlBeginning + escapeHtml(msg) + htmlEnding);
+        $("#messageContainer").append(htmlBeginning + escapeHtml(msg).replace(/\n/g, "<br />") + htmlEnding);
 
         /** Scroll to the bottom of the chat ~ **/
         $("html, body").animate({ scrollTop: $(document).height() });
@@ -139,4 +138,28 @@ $(function() {
          */
         return false;
     };
+
+    /**
+     * Send the message
+     *
+     * @param event
+     * @returns {boolean}
+     */
+    var sendMessage = function(event) {
+        /** Prevent the form from submitting **/
+        event.preventDefault();
+
+        /** Check if the input actually has stuff in it (EG/ not just a bunch of spaces) **/
+        if (0 !== messageInput.val().trim().length) {
+            console.log(messageInput.val())
+            /** Let the app know we want to send the message **/
+            socket.emit("sendchat", messageInput.val());
+
+            /** Clear the input **/
+            messageInput.val("");
+        }
+
+        /** Always return false **/
+        return false;
+    }
 });
